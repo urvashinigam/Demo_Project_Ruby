7e3b7b1c5c1d156e9e5018688db9e0b3b191a297
urvashi <urvashin@shriffle.com>
2022-08-02 19:26:41 +0530

    employee

--- restart_project/employee.rb
+++ restart_project/employee.rb
@@ -33,40 +33,4 @@
           end
           Employee.create("urvashi")
   
-  # -----------------------------------------------------------------------------------------------------
-  #  class Items
-  #   ALL_ITEMS = [] 
-  #   attr_accessor :id, :name, :price
-  #   def initialize(name, price)
-  #     @id = set_incremental_id
-  #     @name = name
-  #     @price = price
-  #     ALL_ITEMS << self
-  #   end
-    
-  #   def set_incremental_id
-  #     (ALL_ITEMS.last&.id || 0) + 1
-  #   end
-
-  #   def destroy
-  #     ALL_ITEMS.delete(self)
-  #   end
-
-  #   class << self
-  #     def create(name, price)
-  #       new(name, price)
-  #     end
-
-  #     def find_by(name)
-  #       all.select {|item| item.name == name}
-  #     end
-
-  #     def all
-  #       ALL_ITEMS
-  #     end
-      
-  #     def clear
-  #       print "\e[2J\e[f"
-  #     end
-  #   end
-  # end
+ 
